b8728285128354c7d0d842d4199c7bddea73f39e
chris <typo3@mediadreams.org>
2025-12-04 07:37:53 +0100

    [!!!][TASK] migrate `list_type` plugins to `CType` and resolve core deprecation #105076
    Deprecation: #105076 - Plugin content element and plugin sub types
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.4/Deprecation-105076-PluginContentElementAndPluginSubTypes.html

---
+++ Classes/Upgrades/PluginListTypeToCTypeUpdate.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mediadreams\MdEventmgtFrontend\Upgrades;
+
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+#[UpgradeWizard('mdeventmgtfrontendExtensionPluginListTypeToCTypeUpdate')]
+final class PluginListTypeToCTypeUpdate extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'mdeventmgtfrontend_frontend' => 'mdeventmgtfrontend_frontend',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'EXT:md_eventmgt_frontend: Migrate plugins';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Migrates mdeventmgtfrontend_frontend from list_type to CType.';
+    }
+}
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,17 +2,24 @@
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$frontendPluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'MdEventmgtFrontend',
     'Frontend',
-    'Event management frontend'
+    'Event management frontend',
+    null,
+    null,
+    'Plugin for manage entries of ext:sf_event_mgt in the frontend.'
 );
 
-// add flexform
-$pluginSignature = 'mdeventmgtfrontend_frontend';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,pi_flexform,pages,recursive',
+    $frontendPluginSignature,
+    'after:palette:headers',
+);
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignature,
-    'FILE:EXT:md_eventmgt_frontend/Configuration/FlexForms/PluginFrontend.xml'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '',
+    'FILE:EXT:md_eventmgt_frontend/Configuration/FlexForms/PluginFrontend.xml',
+    $frontendPluginSignature,
 );
